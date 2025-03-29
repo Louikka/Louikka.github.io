ba0cc4bfbf1006f0c012fad8deda2218ab7bd8c6
Louikka <lolikadimonaa@gmail.com>
2025-03-29 13:38:55 +0300

    c

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,39 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 getGitUserInfo();
+function getGitUserInfo() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const url = 'https://api.github.com/users/Louikka';
+        let resp = yield fetch(url);
+        let d = yield resp.json();
+        document.querySelector('#app .profile_image > img').setAttribute('src', d.avatar_url);
+        document.querySelector('#app .description .who').innerHTML = d.name;
+    });
+}
+;
 getGitUserRepos();
-document.querySelector('#app .contacts .email').addEventListener('click', function (event) {
+function getGitUserRepos() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const url = 'https://api.github.com/users/Louikka/repos';
+        let resp = yield fetch(url);
+        let d = yield resp.json();
+        d.forEach((obj) => {
+            if (~obj.name.search(/\.website\.layout\.example$/i)) {
+                let s = `<div class="d"><a class="project" href="${obj.html_url}" target="_blank">${obj.name}</a></div>`;
+                document.querySelector('#app .projects').insertAdjacentHTML('beforeend', s);
+            }
+            ;
+        });
+    });
+}
+;
+document.querySelector('#app .contacts .email').addEventListener('click', (event) => {
     navigator.clipboard.writeText(event.currentTarget.innerText);
 });
--- scripts/scripts.ts
+++ scripts/scripts.ts
@@ -1,5 +1,54 @@
+interface AnyObject {
+    [key: string]: any
+}
+
+
+
+// request user info
+
 getGitUserInfo();
+
+async function getGitUserInfo() {
+
+    const url = 'https://api.github.com/users/Louikka';
+
+    let resp = await fetch(url);
+    let d: AnyObject = await resp.json();
+
+
+    document.querySelector('#app .profile_image > img').setAttribute('src', d.avatar_url);
+    document.querySelector('#app .description .who').innerHTML = d.name;
+
+};
+
+
+// request all user public repositories
+
 getGitUserRepos();
+
+async function getGitUserRepos() {
+
+    const url = 'https://api.github.com/users/Louikka/repos';
+
+    let resp = await fetch(url);
+    let d: AnyObject[] = await resp.json();
+
+
+    d.forEach((obj) => {
+        if (~obj.name.search(/\.website\.layout\.example$/i)) {
+
+            let s = `<div class="d"><a class="project" href="${obj.html_url}" target="_blank">${obj.name}</a></div>`;
+
+            document.querySelector('#app .projects').insertAdjacentHTML('beforeend', s);
+
+        };
+    });
+
+};
+
+
+
+// other scripts
 
 document.querySelector('#app .contacts .email').addEventListener('click', (event) => {
 
