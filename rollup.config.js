5f4c343ba00c395f6e79e6f5f3578b5e2f3709e4
Louikka <lolikadimonaa@gmail.com>
2025-09-05 14:32:28 +0300

    aoaooooaoaokak

--- rollup.config.js
+++ rollup.config.js
@@ -8,21 +8,38 @@ import autoprefixer from 'autoprefixer';
 import cssnano from 'cssnano';
 
 
-export default {
-    input : 'src/main.ts',
-    output : {
-        file : 'public/bundle.js',
-        format : 'iife',
+
+export default [
+    {
+        input : 'src/global.ts',
+        output : {
+            file : 'public/bundle-global.js',
+            format : 'es',
+        },
+        plugins : [
+            typescript(),
+            terser({
+                module : false,
+            }),
+        ],
+        treeshake : false,
     },
-    plugins : [
-        typescript(),
-        terser(),
-        scss({
-            fileName : 'bundle.css',
-            processor : () => postcss([
-                autoprefixer(),
-                cssnano({ preset : 'default', })
-            ]),
-        }),
-    ],
-};
+    {
+        input : 'src/main.ts',
+        output : {
+            file : 'public/bundle.js',
+            format : 'iife',
+        },
+        plugins : [
+            typescript(),
+            terser(),
+            scss({
+                fileName : 'bundle.css',
+                processor : () => postcss([
+                    autoprefixer(),
+                    cssnano({ preset : 'default', })
+                ]),
+            }),
+        ],
+    },
+];
